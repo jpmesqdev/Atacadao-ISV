4f6c91eb865b28a4b559ba21ca23c58ccf0c0bc6
João Pedro Mesquita <jp.devdroid@gmail.com>
2022-06-08 05:09:34 -0300

    att

---
+++ app/src/main/java/com/devdroid/atacadao_isv/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.devdroid.atacadao_isv.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devdroid.atacadao_isv.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
